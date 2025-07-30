a624f95b1579bf3a7ce742876d7f1651271afff0
Onur Yilmazer <onur.yilmazer1@gmail.com>
2025-07-30 09:47:34 +0200

    first commit

---
+++ src/main/java/com/onuryilmazer/tradereconsystemdb/TradeReconSystemDbApplication.java
@@ -0,0 +1,13 @@
+package com.onuryilmazer.tradereconsystemdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeReconSystemDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeReconSystemDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onuryilmazer/tradereconsystemdb/TradeReconSystemDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onuryilmazer.tradereconsystemdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeReconSystemDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
